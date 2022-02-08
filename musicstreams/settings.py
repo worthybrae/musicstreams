d81e841c833a845074436412c656c16fbff9fb46
worthybrae <worthy@musicplace.io>
2022-02-08 13:47:32 -0500

    Spaces Static Push

--- musicstreams/settings.py
+++ musicstreams/settings.py
@@ -133,9 +133,26 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'assets')
-STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
+USE_SPACES = os.getenv('USE_SPACES') == 'TRUE'
+
+if USE_SPACES:
+    # settings
+    AWS_ACCESS_KEY_ID = os.getenv('AWS_KEY_ID')
+    AWS_SECRET_ACCESS_KEY = os.getenv('AWS_SECRET_KEY')
+    AWS_STORAGE_BUCKET_NAME = os.getenv('AWS_BUCKET_NAME')
+    AWS_DEFAULT_ACL = 'public-read'
+    AWS_S3_ENDPOINT_URL = 'https://nyc3.digitaloceanspaces.com'
+    AWS_S3_OBJECT_PARAMETERS = {'CacheControl': 'max-age=86400'}
+    # static settings
+    AWS_LOCATION = 'static'
+    STATIC_URL = f'https://{AWS_S3_ENDPOINT_URL}/{AWS_LOCATION}/'
+    STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+else:
+    STATIC_URL = '/static/'
+    STATIC_ROOT = BASE_DIR / 'static'
+
+STATICFILES_DIRS = (BASE_DIR / 'static',)
+
 
 
 # Default primary key field type
--- streams/urls.py
+++ streams/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 from . import views
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path("", views.Homepage, name="Homepage"),
@@ -8,4 +10,4 @@ urlpatterns = [
     path("<str:album_spotify_id>/", views.Detailpage, name="Detailpage")
 
 
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
