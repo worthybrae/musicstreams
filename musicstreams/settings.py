790b45daa7c19e2aa6ef8444fb22b4d690afdf4c
worthybrae <worthy@musicplace.io>
2022-02-08 00:25:35 -0500

    updated static files dir

--- musicstreams/settings.py
+++ musicstreams/settings.py
@@ -134,8 +134,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
-
+STATIC_ROOT = str(BASE_DIR.joinpath('staticfiles'))
+STATICFILES_DIRS = (str(BASE_DIR.joinpath('static')),)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
